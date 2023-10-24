a6e994ced47fb513b294a4e7d5684d2b2fdc5657
mariateresepettersson <mariateresepettersson>
2023-10-24 18:41:04 +0100

    Next question button

---
+++ src/components/NextQuestion.jsx
@@ -0,0 +1,17 @@
+import { useQuizStore } from './stores/useQuizStore';
+
+export function NextQuestion() {
+    const quizStore = useQuizStore(); //Initialize the store
+
+    const handleNextQuestion = () => {
+        quizStore.goToNetQuestions(); //Call the got to next question action 
+    };
+
+    return (
+        <div>
+            <button onClick={handleNextQuestion}>
+                Next Question
+            </button>
+        </div>
+    );
+}
