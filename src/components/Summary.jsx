540addb20ce10472efe6c1307a7bbf62f891cb66
Malin <malinlundhe@hotmail.com>
2023-10-26 14:46:41 +0200

    added confetti

--- src/components/Summary.jsx
+++ src/components/Summary.jsx
@@ -1,17 +1,22 @@
-
+import React from 'react';
+import useWindowSize from 'react-use/lib/useWindowSize';
+import "./Summary.css"
 import RestartButton from "./RestartButton";
 import { useQuizStore } from "../stores/useQuizStore";
+import Confetti from 'react-confetti';
 
 export const Summary = () => {
     const answers = useQuizStore((state) => state.answers);
     const numberOfCorrectAnswers = answers.filter((answer) => answer.isCorrect).length;
+    const { width, height } = useWindowSize();
 
     return (
         <div className="summary">
-            <h2>Are you a quiz-master or what?</h2>
-            <p>You've got {numberOfCorrectAnswers}/7 correct answers!</p>
-            <p>Play again?</p>
+            <h2>Savvy Savor</h2>
+            <h3>Congratulations!</h3>
+            <p>You scored {numberOfCorrectAnswers}/7 🌟</p>
             <RestartButton />
+            <Confetti width={width} height={height} />
         </div>
     );
 };
