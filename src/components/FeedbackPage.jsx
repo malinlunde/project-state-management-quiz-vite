68686f850c3a1febc66141db763a8460e9419aa6
mariateresepettersson <mariateresepettersson>
2023-10-27 18:37:50 +0100

    Adding image to feedback and styling to make image round

--- src/components/FeedbackPage.jsx
+++ src/components/FeedbackPage.jsx
@@ -8,7 +8,7 @@ export const FeedbackPage = () => {
   const answer = answers[currentQuestionIndex]
   const isLastQuestion = currentQuestionIndex === questions.length - 1
 
-  
+
   const feedbackText = answer.isCorrect
     ? "That's correct!"
     : `Ooops! You almost got it right! The correct answer is: ${answer.question.options[answer.question.correctAnswerIndex]}`;
@@ -16,10 +16,12 @@ export const FeedbackPage = () => {
 
 
   return (
-    <div>
-      <img src={answer.question.imgURL} alt="Question Image" />
+    <div className='feedback-wrapper'>
+      <div className='answer-img'>
+        <img className="circle-img" src={answer.question.imgURL} alt="Question Image" />
+      </div>
       <p>{feedbackText}</p>
-      <NextQuestion showSammary={isLastQuestion}/>
+      <NextQuestion showSammary={isLastQuestion} />
     </div>
   );
 };
